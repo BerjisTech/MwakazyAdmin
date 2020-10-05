5f11c57c7a6a0fe9719dbee45102f79cf613770f
Sombo <berjistechnologies@gmail.com>
2020-10-05 06:52:38 +0300

    Windows reinstall

---
+++ app/src/main/java/tech/berjis/mwakazyadmin/CategoryExtrasActivity.java
@@ -0,0 +1,13 @@
+package tech.berjis.mwakazyadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoryExtrasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_extras);
+    }
+}
